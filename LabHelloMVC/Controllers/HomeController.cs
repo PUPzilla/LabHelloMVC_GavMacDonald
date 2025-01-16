78284366d550f39f53071119b62e541d67650a9f
Gavin <gavinmacdonald2000@hotmail.com>
2025-01-16 16:32:32 -0400

    Complete

--- LabHelloMVC/Controllers/HomeController.cs
+++ LabHelloMVC/Controllers/HomeController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using LabHelloMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,27 +5,21 @@ namespace LabHelloMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
-
         public IActionResult Index()
         {
-            return View();
+            Person person = new()
+            {
+                FirstName = "Gav",
+                LastName = "MacDonald"
+            };
+            return View(person);
         }
-
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
